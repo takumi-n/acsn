b75dc7d96be3c4ccea69783800b75152c34b6272
takumi-n <ca01068@cartahd.com>
2023-01-22 17:04:23 +0900

    fix: can run on local

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-const webhookHandler = require('./lib/webhookHandler');
-const db = require('@cyclic.sh/dynamodb');
+const webhookApp = require('./lib/webhookApp');
 
 require('dotenv').config();
 
 async function main() {
-  const collection = db.collection('options');
   const opts = {
     baseUrl: process.env.BASE_URL,
     asanaPat: process.env.ASANA_PAT,
     slackWebhookUrl: process.env.SLACK_WEBHOOK_URL,
     workspaceId: process.env.ASANA_WORKSPACE_ID,
     projectId: process.env.ASANA_PROJECT_ID,
-    storeSecret: (secret) => collection.set('asanaWebhookSecret', { secret }),
-    getSecret: () => collection.get('asanaWebhookSecret').then((s) => s.props.secret),
   };
-  const handler = webhookHandler(opts);
+  const app = webhookApp(opts);
 
-  handler.startServer(process.env.PORT || 8888);
-  handler.startWebhook();
+  app.startServer(process.env.PORT || 8080);
+  app.startWebhook();
 }
 
 main();
